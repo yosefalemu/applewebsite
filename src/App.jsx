15c2f147133710a56807081a5c0033765b5e4508
yosefalemu <yosefalemu007@gmail.com>
2024-10-05 11:59:04 +0300

    3d model started

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import Highlight from "./components/Highlight";
+import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Highlight />
+      <Model />
     </main>
   );
 }
