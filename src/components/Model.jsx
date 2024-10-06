15c3911d88f711f517c9636707fd27d594b74393
yosefalemu <yosefalemu007@gmail.com>
2024-10-06 17:18:37 +0300

    model view for the iPhone 15 done

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,25 +1,123 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { useEffect, useRef, useState } from "react";
+import { models, sizes } from "../constants";
+import * as THREE from "three";
 import ModelView from "./ModelView";
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
+import { animateWithGsapTimeline } from "../utils/animate";
 
 const Model = () => {
+  const [size, setSize] = useState("small");
+  const [model, setModel] = useState(models[0]);
+  //camera control for the model view
+  const cameraControlSmall = useRef();
+  const cameraControlLarge = useRef();
+
+  //actual models
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+  //rotation for the models
+  const [smallRotation, setSmallRotaion] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
+
+  const tl = gsap.timeline();
+  useEffect(() => {
+    if (size === "large") {
+      animateWithGsapTimeline(tl, small, smallRotation, "#view1", "#view2", {
+        transform: "translateX(-100%)",
+        duration: 2,
+      });
+    }
+
+    if (size === "small") {
+      animateWithGsapTimeline(tl, large, largeRotation, "#view2", "#view1", {
+        transform: "translateX(0)",
+        duration: 2,
+      });
+    }
+  }, [size]);
   useGSAP(() => {
     gsap.to("#heading", {
-      y: 0,
       opacity: 1,
-      duration: 1,
-      scrollTrigger: { trigger: "#heading" },
+      y: 0,
+      scrollTrigger: { trigger: "#heading", duration: 2 },
     });
   }, []);
   return (
     <section className="common-padding">
       <div className="screen-max-width">
         <h1 className="section-heading" id="heading">
-          Take Closer look.
+          Take a closer look.
         </h1>
         <div className="flex flex-col items-center mt-5">
-          <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-            <ModelView />
+          <div className="w-full h-[75vh] overflow-hidden relative">
+            <ModelView
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
+              setRotationState={setSmallRotaion}
+              item={model}
+              size={size}
+            />
+            <ModelView
+              index={2}
+              groupRef={large}
+              gsapType="view2"
+              controlRef={cameraControlLarge}
+              setRotationState={setLargeRotation}
+              item={model}
+              size={size}
+            />
+            <Canvas
+              className="w-full h-full"
+              style={{
+                position: "fixed",
+                top: 0,
+                bottom: 0,
+                right: 0,
+                left: 0,
+                overflow: "hidden",
+              }}
+              eventSource={document.getElementById("root")}
+            >
+              <View.Port />
+            </Canvas>
+          </div>
+          <div className="mx-auto w-full">
+            <p className="text-sm font-light text-center mb-5">{model.title}</p>
+            <div className="flex-center">
+              <ul className="color-container">
+                {models.map((item, i) => {
+                  let colorFound = item.color[0];
+                  return (
+                    <li
+                      key={i}
+                      className="w-6 h-6 rounded-full mx-2 cursor-pointer"
+                      style={{ backgroundColor: colorFound }}
+                      onClick={() => setModel(item)}
+                    />
+                  );
+                })}
+              </ul>
+              <button className="size-btn-container">
+                {sizes.map(({ label, value }) => (
+                  <span
+                    key={label}
+                    className="size-btn"
+                    style={{
+                      backgroundColor: size === value ? "white" : "transparent",
+                      color: size === value ? "black" : "white",
+                    }}
+                    onClick={() => setSize(value)}
+                  >
+                    {label}
+                  </span>
+                ))}
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,5 +1,64 @@
-const ModelView = () => {
-  return <div>ModelView</div>;
+import {
+  Html,
+  OrbitControls,
+  PerspectiveCamera,
+  View,
+} from "@react-three/drei";
+import Lights from "./Light";
+import { Suspense } from "react";
+import Iphone from "./IPhone";
+import * as THREE from "three";
+
+const ModelView = ({
+  index,
+  groupRef,
+  gsapType,
+  controlRef,
+  setRotationState,
+  item,
+  size,
+}) => {
+  return (
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${
+        index === 2 ? "right-[-100%]" : "right-0"
+      }`}
+    >
+      <ambientLight intensity={0.3} />
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <Lights />
+      <OrbitControls
+        enableZoom={false}
+        enablePan={false}
+        makeDefault
+        ref={controlRef}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.curent.azimuthalAngle())}
+      />
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? "small" : "large"`}
+        position={[0, 0, 0]}
+      >
+        <Suspense
+          fallback={
+            <Html>
+              <div>Loading...</div>
+            </Html>
+          }
+        >
+          <Iphone
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
+    </View>
+  );
 };
 
 export default ModelView;
